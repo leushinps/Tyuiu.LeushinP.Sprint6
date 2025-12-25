5300b74826e60e7739dc74dc8a480cdb6e62b192
Pavel Leushin <leushinps@outlook.com>
2025-12-26 04:40:19 +0500

    Task0

--- Tyuiu.LeushinP.Sprint6.Task0.V5/FormMain.Designer.cs
+++ Tyuiu.LeushinP.Sprint6.Task0.V5/FormMain.Designer.cs
@@ -44,7 +44,7 @@ namespace Tyuiu.LeushinP.Sprint6.Task0.V5
             groupBoxOutput_LPS = new GroupBox();
             textBoxOutputEnd_LPS = new TextBox();
             textBoxOutpunStart_LPS = new TextBox();
-            groupBoxTask_PPE.SuspendLayout();
+            groupBoxTask_LPS.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBoxFarmula_LPS).BeginInit();
             groupBoxInput_LPS.SuspendLayout();
             groupBoxOutput_LPS.SuspendLayout();
@@ -52,136 +52,136 @@ namespace Tyuiu.LeushinP.Sprint6.Task0.V5
             // 
             // buttonHelp_MSM
             // 
-            buttonHelp_PPE.FlatStyle = FlatStyle.Flat;
-            buttonHelp_PPE.Font = new Font("Segoe UI", 14F);
-            buttonHelp_PPE.Location = new Point(656, 520);
-            buttonHelp_PPE.Margin = new Padding(3, 4, 3, 4);
-            buttonHelp_PPE.Name = "buttonHelp_MSM";
-            buttonHelp_PPE.Size = new Size(57, 67);
-            buttonHelp_PPE.TabIndex = 0;
-            buttonHelp_PPE.Text = "?";
-            buttonHelp_PPE.UseVisualStyleBackColor = true;
-            buttonHelp_PPE.Click += buttonHelp_PPE_Click;
+            buttonHelp_LPS.FlatStyle = FlatStyle.Flat;
+            buttonHelp_LPS.Font = new Font("Segoe UI", 14F);
+            buttonHelp_LPS.Location = new Point(656, 520);
+            buttonHelp_LPS.Margin = new Padding(3, 4, 3, 4);
+            buttonHelp_LPS.Name = "buttonHelp_MSM";
+            buttonHelp_LPS.Size = new Size(57, 67);
+            buttonHelp_LPS.TabIndex = 0;
+            buttonHelp_LPS.Text = "?";
+            buttonHelp_LPS.UseVisualStyleBackColor = true;
+            buttonHelp_LPS.Click += buttonHelp_LPS_Click;
             // 
             // buttonResult_MSM
             // 
-            buttonResult_PPE.BackColor = SystemColors.Control;
-            buttonResult_PPE.Location = new Point(720, 520);
-            buttonResult_PPE.Margin = new Padding(3, 4, 3, 4);
-            buttonResult_PPE.Name = "buttonResult_MSM";
-            buttonResult_PPE.Size = new Size(181, 64);
-            buttonResult_PPE.TabIndex = 1;
-            buttonResult_PPE.Text = "Выполнить";
-            buttonResult_PPE.UseVisualStyleBackColor = false;
-            buttonResult_PPE.Click += buttonResult_PPE_Click;
+            buttonResult_LPS.BackColor = SystemColors.Control;
+            buttonResult_LPS.Location = new Point(720, 520);
+            buttonResult_LPS.Margin = new Padding(3, 4, 3, 4);
+            buttonResult_LPS.Name = "buttonResult_MSM";
+            buttonResult_LPS.Size = new Size(181, 64);
+            buttonResult_LPS.TabIndex = 1;
+            buttonResult_LPS.Text = "Выполнить";
+            buttonResult_LPS.UseVisualStyleBackColor = false;
+            buttonResult_LPS.Click += buttonResult_LPS_Click;
             // 
             // groupBoxTask_MSM
             // 
-            groupBoxTask_PPE.Controls.Add(textBoxTask_MSM);
-            groupBoxTask_PPE.Controls.Add(pictureBoxFarmula_MSM);
-            groupBoxTask_PPE.Location = new Point(14, 16);
-            groupBoxTask_PPE.Margin = new Padding(3, 4, 3, 4);
-            groupBoxTask_PPE.Name = "groupBoxTask_MSM";
-            groupBoxTask_PPE.Padding = new Padding(3, 4, 3, 4);
-            groupBoxTask_PPE.Size = new Size(887, 351);
-            groupBoxTask_PPE.TabIndex = 2;
-            groupBoxTask_PPE.TabStop = false;
-            groupBoxTask_PPE.Text = "Условие";
+            groupBoxTask_LPS.Controls.Add(textBoxTask_MSM);
+            groupBoxTask_LPS.Controls.Add(pictureBoxFarmula_MSM);
+            groupBoxTask_LPS.Location = new Point(14, 16);
+            groupBoxTask_LPS.Margin = new Padding(3, 4, 3, 4);
+            groupBoxTask_LPS.Name = "groupBoxTask_MSM";
+            groupBoxTask_LPS.Padding = new Padding(3, 4, 3, 4);
+            groupBoxTask_LPS.Size = new Size(887, 351);
+            groupBoxTask_LPS.TabIndex = 2;
+            groupBoxTask_LPS.TabStop = false;
+            groupBoxTask_LPS.Text = "Условие";
             // 
             // textBoxTask_MSM
             // 
-            textBoxTask_PPE.BackColor = SystemColors.Control;
-            textBoxTask_PPE.BorderStyle = BorderStyle.None;
-            textBoxTask_PPE.Location = new Point(7, 29);
-            textBoxTask_PPE.Margin = new Padding(3, 4, 3, 4);
-            textBoxTask_PPE.Multiline = true;
-            textBoxTask_PPE.Name = "textBoxTask_LPS";
-            textBoxTask_PPE.ReadOnly = true;
-            textBoxTask_PPE.Size = new Size(527, 313);
-            textBoxTask_PPE.TabIndex = 1;
-            textBoxTask_PPE.Text = "Вычислить выражение по формуле";
+            textBoxTask_LPS.BackColor = SystemColors.Control;
+            textBoxTask_LPS.BorderStyle = BorderStyle.None;
+            textBoxTask_LPS.Location = new Point(7, 29);
+            textBoxTask_LPS.Margin = new Padding(3, 4, 3, 4);
+            textBoxTask_LPS.Multiline = true;
+            textBoxTask_LPS.Name = "textBoxTask_LPS";
+            textBoxTask_LPS.ReadOnly = true;
+            textBoxTask_LPS.Size = new Size(527, 313);
+            textBoxTask_LPS.TabIndex = 1;
+            textBoxTask_LPS.Text = "Вычислить выражение по формуле";
             // 
             // pictureBoxFarmula_MSM
             // 
-            pictureBoxFarmula_PPE.BackColor = SystemColors.Window;
-            pictureBoxFarmula_PPE.Location = new Point(541, 29);
-            pictureBoxFarmula_PPE.Margin = new Padding(3, 4, 3, 4);
-            pictureBoxFarmula_PPE.Name = "pictureBoxFarmula_LPS";
-            pictureBoxFarmula_PPE.Size = new Size(339, 52);
-            pictureBoxFarmula_PPE.TabIndex = 0;
-            pictureBoxFarmula_PPE.TabStop = false;
-            pictureBoxFarmula_PPE.Click += this.pictureBoxFarmula_LPS_Click;
+            pictureBoxFarmula_LPS.BackColor = SystemColors.Window;
+            pictureBoxFarmula_LPS.Location = new Point(541, 29);
+            pictureBoxFarmula_LPS.Margin = new Padding(3, 4, 3, 4);
+            pictureBoxFarmula_LPS.Name = "pictureBoxFarmula_LPS";
+            pictureBoxFarmula_LPS.Size = new Size(339, 52);
+            pictureBoxFarmula_LPS.TabIndex = 0;
+            pictureBoxFarmula_LPS.TabStop = false;
+            pictureBoxFarmula_LPS.Click += this.pictureBoxFarmula_LPS_Click;
             // 
             // groupBoxInput_MSM
             // 
-            groupBoxInput_PPE.Controls.Add(textBoxInputEnd_LPS);
-            groupBoxInput_PPE.Controls.Add(textBoxInputStart_LPS);
-            groupBoxInput_PPE.Location = new Point(14, 375);
-            groupBoxInput_PPE.Margin = new Padding(3, 4, 3, 4);
-            groupBoxInput_PPE.Name = "groupBoxInput_MSM";
-            groupBoxInput_PPE.Padding = new Padding(3, 4, 3, 4);
-            groupBoxInput_PPE.Size = new Size(638, 127);
-            groupBoxInput_PPE.TabIndex = 3;
-            groupBoxInput_PPE.TabStop = false;
-            groupBoxInput_PPE.Text = "Ввод данных:";
+            groupBoxInput_LPS.Controls.Add(textBoxInputEnd_LPS);
+            groupBoxInput_LPS.Controls.Add(textBoxInputStart_LPS);
+            groupBoxInput_LPS.Location = new Point(14, 375);
+            groupBoxInput_LPS.Margin = new Padding(3, 4, 3, 4);
+            groupBoxInput_LPS.Name = "groupBoxInput_MSM";
+            groupBoxInput_LPS.Padding = new Padding(3, 4, 3, 4);
+            groupBoxInput_LPS.Size = new Size(638, 127);
+            groupBoxInput_LPS.TabIndex = 3;
+            groupBoxInput_LPS.TabStop = false;
+            groupBoxInput_LPS.Text = "Ввод данных:";
             // 
             // textBoxInputEnd_MSM
             // 
-            textBoxInputEnd_PPE.Font = new Font("Segoe UI", 16F);
-            textBoxInputEnd_PPE.Location = new Point(7, 68);
-            textBoxInputEnd_PPE.Margin = new Padding(3, 4, 3, 4);
-            textBoxInputEnd_PPE.Name = "textBoxInputEnd_LPS";
-            textBoxInputEnd_PPE.Size = new Size(623, 43);
-            textBoxInputEnd_PPE.TabIndex = 1;
-            textBoxInputEnd_PPE.KeyPress += textBoxInputEnd_MSM_KeyPress;
+            textBoxInputEnd_LPS.Font = new Font("Segoe UI", 16F);
+            textBoxInputEnd_LPS.Location = new Point(7, 68);
+            textBoxInputEnd_LPS.Margin = new Padding(3, 4, 3, 4);
+            textBoxInputEnd_LPS.Name = "textBoxInputEnd_LPS";
+            textBoxInputEnd_LPS.Size = new Size(623, 43);
+            textBoxInputEnd_LPS.TabIndex = 1;
+            textBoxInputEnd_LPS.KeyPress += textBoxInputEnd_MSM_KeyPress;
             // 
             // textBoxInputStart_MSM
             // 
-            textBoxInputStart_PPE.BackColor = SystemColors.Control;
-            textBoxInputStart_PPE.BorderStyle = BorderStyle.None;
-            textBoxInputStart_PPE.Location = new Point(7, 39);
-            textBoxInputStart_PPE.Margin = new Padding(3, 4, 3, 4);
-            textBoxInputStart_PPE.Name = "textBoxInputStart_LPS";
-            textBoxInputStart_PPE.ReadOnly = true;
-            textBoxInputStart_PPE.Size = new Size(114, 20);
-            textBoxInputStart_PPE.TabIndex = 0;
-            textBoxInputStart_PPE.Text = "Переменная X:";
+            textBoxInputStart_LPS.BackColor = SystemColors.Control;
+            textBoxInputStart_LPS.BorderStyle = BorderStyle.None;
+            textBoxInputStart_LPS.Location = new Point(7, 39);
+            textBoxInputStart_LPS.Margin = new Padding(3, 4, 3, 4);
+            textBoxInputStart_LPS.Name = "textBoxInputStart_LPS";
+            textBoxInputStart_LPS.ReadOnly = true;
+            textBoxInputStart_LPS.Size = new Size(114, 20);
+            textBoxInputStart_LPS.TabIndex = 0;
+            textBoxInputStart_LPS.Text = "Переменная X:";
             // 
             // groupBoxOutput_MSM
             // 
-            groupBoxOutput_PPE.Controls.Add(textBoxOutputEnd_PPE);
-            groupBoxOutput_PPE.Controls.Add(textBoxOutpunStart_PPE);
-            groupBoxOutput_PPE.Location = new Point(658, 375);
-            groupBoxOutput_PPE.Margin = new Padding(3, 4, 3, 4);
-            groupBoxOutput_PPE.Name = "groupBoxOutput_PPE";
-            groupBoxOutput_PPE.Padding = new Padding(3, 4, 3, 4);
-            groupBoxOutput_PPE.Size = new Size(242, 127);
-            groupBoxOutput_PPE.TabIndex = 4;
-            groupBoxOutput_PPE.TabStop = false;
-            groupBoxOutput_PPE.Text = "Вывод данных:";
+            groupBoxOutput_LPS.Controls.Add(textBoxOutputEnd_PPE);
+            groupBoxOutput_LPS.Controls.Add(textBoxOutpunStart_PPE);
+            groupBoxOutput_LPS.Location = new Point(658, 375);
+            groupBoxOutput_LPS.Margin = new Padding(3, 4, 3, 4);
+            groupBoxOutput_LPS.Name = "groupBoxOutput_PPE";
+            groupBoxOutput_LPS Padding = new Padding(3, 4, 3, 4);
+            groupBoxOutput_LPS.Size = new Size(242, 127);
+            groupBoxOutput_LPS.TabIndex = 4;
+            groupBoxOutput_LPS.TabStop = false;
+            groupBoxOutput_LPS.Text = "Вывод данных:";
             // 
             // textBoxOutputEnd_MSM
             // 
-            groupBoxOutput_PPE.Font = new Font("Segoe UI", 16F);
-            groupBoxOutput_PPE.Location = new Point(7, 68);
-            groupBoxOutput_PPE.Margin = new Padding(3, 4, 3, 4);
-            groupBoxOutput_PPE.Name = "textBoxOutputEnd_PPE";
-            groupBoxOutput_PPE.ReadOnly = true;
-            groupBoxOutput_PPE.Size = new Size(207, 43);
-            groupBoxOutput_PPE.TabIndex = 1;
+            groupBoxOutput_LPS.Font = new Font("Segoe UI", 16F);
+            groupBoxOutput_LPS.Location = new Point(7, 68);
+            groupBoxOutput_LPS.Margin = new Padding(3, 4, 3, 4);
+            groupBoxOutput_LPS.Name = "textBoxOutputEnd_LPS";
+            groupBoxOutput_LPS.ReadOnly = true;
+            groupBoxOutput_LPS.Size = new Size(207, 43);
+            groupBoxOutput_LPS.TabIndex = 1;
             // 
             // textBoxOutpunStart_MSM
             // 
-            textBoxOutpunStart_PPE.BackColor = SystemColors.Control;
-            textBoxOutpunStart_PPE.BorderStyle = BorderStyle.None;
-            textBoxOutpunStart_PPE.Location = new Point(7, 39);
-            textBoxOutpunStart_PPE.Margin = new Padding(3, 4, 3, 4);
-            textBoxOutpunStart_PPE.Multiline = true;
-            textBoxOutpunStart_PPE.Name = "textBoxOutpunStart_PPE";
-            textBoxOutpunStart_PPE.ReadOnly = true;
-            textBoxOutpunStart_PPE.Size = new Size(114, 21);
-            textBoxOutpunStart_PPE.TabIndex = 0;
-            textBoxOutpunStart_PPE.Text = "Результат:";
+            textBoxOutpunStart_LPS.BackColor = SystemColors.Control;
+            textBoxOutpunStart_LPS.BorderStyle = BorderStyle.None;
+            textBoxOutpunStart_LPS.Location = new Point(7, 39);
+            textBoxOutpunStart_LPS.Margin = new Padding(3, 4, 3, 4);
+            textBoxOutpunStart_LPS.Multiline = true;
+            textBoxOutpunStart_LPS.Name = "textBoxOutpunStart_LPS";
+            textBoxOutpunStart_LPS.ReadOnly = true;
+            textBoxOutpunStart_LPS.Size = new Size(114, 21);
+            textBoxOutpunStart_LPS.TabIndex = 0;
+            textBoxOutpunStart_LPS.Text = "Результат:";
             // 
             // FormMain
             // 
@@ -202,10 +202,10 @@ namespace Tyuiu.LeushinP.Sprint6.Task0.V5
             groupBoxTask_LPS.ResumeLayout(false);
             groupBoxTask_LPS.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBoxFarmula_LPS).EndInit();
-            groupBoxInput_PPE.ResumeLayout(false);
-            groupBoxInput_PPE.PerformLayout();
-            groupBoxOutput_PPE.ResumeLayout(false);
-            groupBoxOutput_PPE.PerformLayout();
+            groupBoxInput_LPS.ResumeLayout(false);
+            groupBoxInput_LPS.PerformLayout();
+            groupBoxOutput_LPS.ResumeLayout(false);
+            groupBoxOutput_LPS.PerformLayout();
             ResumeLayout(false);
         }
 
--- Tyuiu.LeushinP.Sprint6.Task0.V5/FormMain.cs
+++ Tyuiu.LeushinP.Sprint6.Task0.V5/FormMain.cs
@@ -9,23 +9,23 @@ namespace Tyuiu.LeushinP.Sprint6.Task0.V5
             InitializeComponent();
         }
 
-        private void buttonResult_PPE_Click(object sender, EventArgs e)
+        private void buttonResult_LPS_Click(object sender, EventArgs e)
         {
             DataService ds = new DataService();
             try
             {
-                textBoxOutputEnd_PPE.Text = Convert.ToString(ds.Calculate(Convert.ToInt32(textBoxInputEnd_PPE.Text)));
+                textBoxOutputEnd_LPS.Text = Convert.ToString(ds.Calculate(Convert.ToInt32(textBoxInputEnd_LPS.Text)));
             }
             catch
             {
                 MessageBox.Show("-", "-", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
-        private void textBoxInputEnd_PPE_KeyPress(object sender, KeyPressEventArgs e)
+        private void textBoxInputEnd_LPS_KeyPress(object sender, KeyPressEventArgs e)
         {
             if ((e.KeyChar <= 47 || e.KeyChar >= 58) && (e.KeyChar != ',') && (e.KeyChar != 8)) e.Handled = true;
         }
-        private void buttonHelp_PPE_Click(object sender, EventArgs e)
+        private void buttonHelp_LPS_Click(object sender, EventArgs e)
         {
             MessageBox.Show("-", "-");
         }
